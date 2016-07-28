a14741dd7884ae331893389773313faa2d22b3f7
Lihao <591353643@qq.com>
2016-07-28 22:23:51 +0800

    first edit 20160728-2

--- java/com/lihao/haogenews/views/BaseFragment.java
+++ java/com/lihao/haogenews/views/BaseFragment.java
@@ -1,9 +1,45 @@
 package com.lihao.haogenews.views;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lihao.haogenews.utils.Constants;
 
 /**
  * Created by Administrator on 2016/7/28.
  */
-public class BaseFragment extends Fragment{
+public abstract class BaseFragment extends Fragment{
+    protected Activity mActivity;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mActivity = getActivity();
+        return initView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    protected void initData() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mActivity = null;
+        Log.d(Constants.APP_NAME,"A Fragment has bean Destroyed!!!");
+    }
+
+    protected abstract View initView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState);
 }
--- java/com/lihao/haogenews/views/MainFragment.java
+++ java/com/lihao/haogenews/views/MainFragment.java
@@ -1,6 +1,10 @@
 package com.lihao.haogenews.views;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 /**
@@ -9,4 +13,8 @@ import android.widget.BaseAdapter;
 public class MainFragment extends BaseFragment{
 
 
+    @Override
+    protected View initView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
 }
