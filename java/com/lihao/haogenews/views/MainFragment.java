011e442f896486137367b72585df4af0b45f1b52
Lihao <591353643@qq.com>
2016-07-29 14:45:09 +0800

    second edit 0729

--- java/com/lihao/haogenews/views/MainFragment.java
+++ java/com/lihao/haogenews/views/MainFragment.java
@@ -6,15 +6,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import com.lihao.haogenews.R;
+import com.youth.banner.Banner;
 
 /**
  * Created by Administrator on 2016/7/28.
  */
 public class MainFragment extends BaseFragment{
 
+    private ListView mListView;
+    private Banner mBanner;
+
+
 
     @Override
     protected View initView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return null;
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+        mListView = (ListView) view.findViewById(R.id.lv_main);
+        View headView = inflater.inflate(R.layout.main_banner,mListView,false);
+        mBanner = (Banner) headView.findViewById(R.id.banner);
+        setBanner();
+        mListView.addHeaderView(headView);
+        return view;
+    }
+
+    private void setBanner() {
     }
 }
